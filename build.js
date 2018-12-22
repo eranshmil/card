f27d9485ae1a62231d882ba2add05c3f342a7225
eranshmil <eranshmil@users.noreply.github.com>
2018-12-22 23:10:48 +0200

    refactor + build card to remove deps

---
+++ build.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const generateCards = require('./card');
+
+const data = {
+  avatarFilePath: path.join(__dirname, 'assets/avatar.png'),
+  name: 'Eran Shmil',
+  work: 'Full-Stack Developer',
+  email: 'eranshmil@gmail.com',
+  twitter: 'https://twitter.com/eranshmil',
+  github: 'https://github.com/eranshmil',
+  linkedin: 'https://linkedin.com/in/eranshmil',
+  npx: 'npx @eranshmil/card [--fg]',
+};
+
+/**
+ * Image To Ascii configurations.
+ *
+ * https://github.com/IonicaBizau/image-to-ascii#memo-documentation
+ */
+const imageToAscii = {
+  size: {
+    width: 20,
+    height: 20,
+  },
+};
+
+/**
+ * Box configurations.
+ *
+ * https://github.com/sindresorhus/boxen#api
+ */
+const boxen = {
+  padding: 1,
+  float: 'center',
+  borderStyle: 'round',
+};
+
+generateCards(data, imageToAscii, boxen);
---
+++ card.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+
+const chalk = require('chalk');
+const boxen = require('boxen');
+const imageToAscii = require('image-to-ascii');
+
+/**
+ * Texts template that will be placed to the right of the avatar.
+ *
+ * @param {object} data Card information.
+ *
+ * @returns {string[]}
+ */
+function getCardTextTemplate(data) {
+  return chalk`
+    {green.bold ${data.name}}\n
+    {white.italic.bold ${data.work}}\n\n\n
+    {white.bold Email:}     {cyan ${data.email}}\n
+    {white.bold Twitter:}   {cyan ${data.twitter}}\n
+    {white.bold GitHub:}    {cyan ${data.github}}\n
+    {white.bold LinkedIn:}  {cyan ${data.linkedin}}\n\n\n\n
+    {white.bold Card:}      {white ${data.npx}}
+    `.split('\n');
+}
+
+/**
+ * Convert an image to colored text ascii.
+ *
+ * @param {string} filePath Image file location.
+ * @param {boolean} useBgColor Convert image using background color.
+ *
+ * @returns {string[]}
+ */
+async function convertImageToAscii(filePath, imageToAsciiOptions) {
+  return new Promise((resolve, reject) => {
+    imageToAscii(filePath, imageToAsciiOptions, (err, output) => {
+      if (err) return reject(err);
+
+      return resolve(output.split('\n'));
+    });
+  });
+}
+
+/**
+ * Convert the avatar and merge with the supplied information.
+ *
+ * @param {object} data Card information.
+ * @param {object} imageToAsciiOptions
+ * @param {object} boxenOptions
+ *
+ * @returns {string}
+ */
+async function createCard(data, imageToAsciiOptions, boxenOptions) {
+  const avatar = await convertImageToAscii(data.avatarFilePath, imageToAsciiOptions);
+  const cardTextTemplate = getCardTextTemplate(data);
+
+  const mergedOutput = avatar
+    .map((line, index) => line.concat(cardTextTemplate[index] || ''))
+    .join('\n');
+
+  return chalk.green(boxen(mergedOutput, boxenOptions));
+}
+
+/**
+ * Generate two versions of the card and save to bin folder.
+ *
+ * @param {object} data Card information.
+ * @param {object} imageToAsciiOptions
+ * @param {object} boxenOptions
+ */
+async function generateCards(data, imageToAsciiOptions, boxenOptions) {
+  try {
+    const outputFg = await createCard(data, imageToAsciiOptions, boxenOptions);
+    fs.writeFileSync(path.join(__dirname, 'bin/output-fg'), outputFg);
+
+    const output = await createCard(
+      data,
+      { ...imageToAsciiOptions, bg: true, pixels: ' ' },
+      boxenOptions,
+    );
+    fs.writeFileSync(path.join(__dirname, 'bin/output'), output);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = generateCards;
